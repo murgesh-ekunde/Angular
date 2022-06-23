116f492ebb84da1a2c3573b3255398c6b1bb871d
Murgesh Ekunde <mekunde@gmail.com>
2022-06-23 17:08:43 +0530

    Custom pipe example 1

---
+++ app/my-add.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'myAdd'
+})
+export class MyAddPipe implements PipeTransform {
+
+  transform(value: any, no: any): unknown 
+  {
+    return value + no ;
+
+}
+}
